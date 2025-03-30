778a481b7ece4ff0620b299393952f8de05f0b52
HaseebQureshi7 <qureshihaxeeb2@gmail.com>
2025-03-31 04:23:10 +0530

    chore: Render URL added

--- src/config/baseURL.ts
+++ src/config/baseURL.ts
@@ -1,2 +1,7 @@
-export const baseURL = "http://localhost:3000";
-export const socketURL = "ws://localhost:3000";
+// --> dev
+// export const baseURL = "http://localhost:3000";
+// export const socketURL = "ws://localhost:3000";
+
+// --> prod
+export const baseURL = "https://huddle-server-45lp.onrender.com";
+export const socketURL = "ws://huddle-server-45lp.onrender.com";
