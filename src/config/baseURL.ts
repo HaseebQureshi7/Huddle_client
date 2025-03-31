f058135e14a0f4f539deec743c37170dc0882be2
HaseebQureshi7 <qureshihaxeeb2@gmail.com>
2025-04-01 04:44:45 +0530

    chore: changed Local URLS to deployment URLs

--- src/config/baseURL.ts
+++ src/config/baseURL.ts
@@ -1,7 +1,7 @@
 // --> dev
-export const baseURL = "https://localhost:3000";
-export const socketURL = "wss://localhost:3000";
+// export const baseURL = "https://localhost:3000";
+// export const socketURL = "wss://localhost:3000";
 
 // --> prod
-// export const baseURL = "https://huddle-server-0yw2.onrender.com";
-// export const socketURL = "ws://huddle-server-0yw2.onrender.com";
+export const baseURL = "https://huddle-server-0yw2.onrender.com";
+export const socketURL = "wss://huddle-server-0yw2.onrender.com";
